07d8c5cf1d184048c5b3d24c9e27cefb54026390
IsaacNewtonTingo <newtone480@gmail.com>
2022-02-21 16:58:33 +0300

    Reset password add section

--- FXBinaryProject/info.py
+++ FXBinaryProject/info.py
@@ -6,3 +6,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'fxbinaryinfotech@gmail.com'
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('signup', views.signup, name="signup"),
@@ -15,6 +17,26 @@ urlpatterns = [
     path('deposit', views.deposit, name="deposit"),
     path('withdraw', views.withdraw, name="withdraw"),
     path('mpesaWithdrawal', views.mpesaWithdrawal, name="mpesaWithdrawal"),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(
+             template_name="authentication/password_reset.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="authentication/password_reset_sent.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="authentication/password_reset_form.html"),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="authentication/password_reset_done.html"),
+         name="password_reset_complete"),
 
 
 ]
